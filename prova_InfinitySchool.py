0817199005930271564ddcfb5e7de827eeaa6bbb
antonioCalazansRedShelf <antonio.calazans@redshelf.com>
2023-10-28 00:09:39 -0300

    Finalização da prova

---
+++ prova_InfinitySchool.py
@@ -0,0 +1,28 @@
+##Primeira prova referente a variáveis
+
+#Questão 01
+"""
+Pergunta: 
+[LP-A01] A partir do que aprendemos até agora:
+Crie um exemplo de cada uma das principais tipagens de variáveis.
+Crie uma solicitação de dados ao usuário.
+Imprima a solicitação anterior na tela com uma mensagem personalizada.
+"""
+
+#Exemplo tipagem: Int 
+idade = 33
+
+#Exemplo tipagem: Float
+altura = 1.85
+
+#Exemplo tipagem: String
+nome_do_usuario = "Antonio Calazans"
+
+#Exemplo de tipagem: Boolean
+maior_de_idade = True
+
+#Criando uma solicitação para o usuario:
+nome_do_aluno = input("Digite aqui o seu nome: ")
+
+#Imprimindo informação de acordo com o valor indicado no item anterior
+print(f"Olá {nome_do_aluno}, você acabou de mandar a sua prova para ser avaliada por um dos nossos professores da Infinity School")
