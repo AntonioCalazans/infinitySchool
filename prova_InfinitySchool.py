68643329957bf5afadb626e1694401e244faec11
antonioCalazansRedShelf <antonio.calazans@redshelf.com>
2023-10-29 17:53:49 -0300

    Questao da prova - Logica

--- prova_InfinitySchool.py
+++ prova_InfinitySchool.py
@@ -1,28 +1,22 @@
-##Primeira prova referente a variáveis
+##Prova sobre condicionais
 
 #Questão 01
 """
-Pergunta: 
-[LP-A01] A partir do que aprendemos até agora:
-Crie um exemplo de cada uma das principais tipagens de variáveis.
-Crie uma solicitação de dados ao usuário.
-Imprima a solicitação anterior na tela com uma mensagem personalizada.
+[LP-A02] Escreva um programa em python que pergunte ao usuário a velocidade de um carro. 
+Caso ultrapasse 80 km/h, exiba uma mensagem dizendo que o usuário foi multado. 
+Nesse caso, exiba o valor da multa, cobrando R$20,00 por cada km que exceder 80 km/h.
 """
 
-#Exemplo tipagem: Int 
-idade = 33
+velocidade_carro = float(input("Digite a velocidade do carro em km/h: "))
+limite_velocidade = 80
+#Declara o valor da multa
+valor_da_multa = 20
 
-#Exemplo tipagem: Float
-altura = 1.85
+if velocidade_carro > limite_velocidade:
+    # Calcula a multa
+    multa = (velocidade_carro - limite_velocidade) * valor_da_multa
 
-#Exemplo tipagem: String
-nome_do_usuario = "Antonio Calazans"
-
-#Exemplo de tipagem: Boolean
-maior_de_idade = True
-
-#Criando uma solicitação para o usuario:
-nome_do_aluno = input("Digite aqui o seu nome: ")
-
-#Imprimindo informação de acordo com o valor indicado no item anterior
-print(f"Olá {nome_do_aluno}, você acabou de mandar a sua prova para ser avaliada por um dos nossos professores da Infinity School")
+    print(f"Você foi multado! Velocidade acima do limite permitido de {limite_velocidade} km/h.")
+    print(f"Valor da multa: R$ {multa:.2f}")
+else:
+    print("Você está dentro do limite de velocidade. Dirija com segurança!")
